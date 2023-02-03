5951ae311014d8f0ee0176de139c8bfa0880fa09
lml <m.luo1900@gmail.com>
2023-02-03 19:13:35 +0800

    add comment

--- 444SequenceReconstruction/seqReconstruction.cpp
+++ 444SequenceReconstruction/seqReconstruction.cpp
@@ -5,6 +5,8 @@ public:
         vector<vector<int>> graph(n + 1);
         vector<unordered_set<int>> visited(n + 1);
         vector<int> indegree(n + 1, 0);
+        // 根据序列建立有向图，每个序列的相邻两个数字代表图中节点的先后顺序
+        // 利用set排除重边，用indegree记录每个节点的入度
         for (auto seq : sequences)
         {
             for (int i = 0; i < seq.size() - 1; i++)
@@ -18,6 +20,7 @@ public:
                 }
             }
         }
+        // 拓扑排序
         queue<int> q;
         vector<int> ans;
         for (int i = 1; i <= n; i++)
@@ -31,6 +34,7 @@ public:
         while (!q.empty())
         {
             int qsize = q.size();
+            // 如果队列大小大于1，说明该拓扑排序不唯一，不存在唯一超序列
             if (qsize > 1) return false;
             int id = q.front();
             q.pop();
@@ -44,6 +48,7 @@ public:
                 }
             }
         }
+        // 如果nums与拓扑排序结果不符合，说明其不是最短超序列
         if (ans.size() != n) return false;
         for (int i = 0; i < n; i++)
         {
